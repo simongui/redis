30a1422fe8ebc6ce564e49abbbbf54da66867c0b
Simon Guindon <simon@nureality.ca>
2016-10-06 14:34:01 -0400

    Changed latency results from millisecond resolution to microsecond resolution.

--- src/redis-benchmark.c
+++ src/redis-benchmark.c
@@ -444,10 +444,10 @@ static void showLatencyReport(void) {
 
         qsort(config.latency,config.requests,sizeof(long long),compareLatency);
         for (i = 0; i < config.requests; i++) {
-            if (config.latency[i]/1000 != curlat || i == (config.requests-1)) {
-                curlat = config.latency[i]/1000;
+            if (config.latency[i] != curlat || i == (config.requests-1)) {
+                curlat = config.latency[i];
                 perc = ((float)(i+1)*100)/config.requests;
-                printf("%.2f%% <= %d milliseconds\n", perc, curlat);
+                printf("%.2f%% <= %d microseconds\n", perc, curlat);
             }
         }
         printf("%.2f requests per second\n\n", reqpersec);
